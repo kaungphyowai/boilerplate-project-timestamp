109363da2211ff7879bb8943c9edc266fc8550bc
Kaung-Phyo-Wai <kaungphyowaidecember@gmail.com>
2022-01-07 18:31:58 +0630

    complete

--- server.js
+++ server.js
@@ -24,15 +24,26 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
+app.get("/api", function (req, res) {
+  let now = new Date();
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString(),
+  });
+});
+
 app.get("/api/:date", (req, res) => {
+  const times = req.params.date;
   try {
-    let date = req.params.date.includes("-")
-      ? new Date(req.params.date)
-      : new Date(parseInt(req.params.date));
-    res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString(),
-    });
+    let date = /^\d+$/.test(times)
+      ? new Date(parseInt(times))
+      : new Date(times);
+    date == "Invalid Date"
+      ? res.json({ error: "Invalid Date" })
+      : res.json({
+          unix: date.getTime(),
+          utc: date.toUTCString(),
+        });
   } catch (error) {
     res.json({ error: error });
   }
